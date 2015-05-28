d8dbff7a390e065d75773414fd4703776a09c77a
Andrius Aucinas <andrius@smart-e.org>
2015-05-28 11:16:40 +0100

    added facebook post example to be worked into the db

---
+++ src/test/scala/FacebookPostExample.js
@@ -0,0 +1,471 @@
+{
+    "id": "19292868552_10150189643478553",
+    "from": {
+    "category": "Product/Service",
+        "name": "Facebook Developers",
+        "id": "19292868552"
+},
+    "picture": "https://fbexternal-a.akamaihd.net/safe_image.php?d=AQA9UX6jtOYVwALB&w=130&h=130&url=http%3A%2F%2Fimg.bitpixels.com%2Fgetthumbnail%3Fcode%3D78793%26size%3D120%26url%3Dhttp%253A%252F%252Fdevelopers.facebook.com%252Fblog%252F&cfs=1",
+    "link": "http://developers.facebook.com/blog/post/497",
+    "name": "Developer Roadmap Update: Moving to OAuth 2.0 + HTTPS",
+    "description": "\nWe continue to make Platform more secure for users. Earlier this year, we introduced the ability for users to browse Facebook over HTTPS. As a result, we provided “Secure Canvas URL” and “Secure Tab URL” fields in the Developer App for developers to serve their apps through an H",
+    "icon": "https://www.facebook.com/images/icons/post.gif",
+    "actions": [
+    {
+        "name": "Comment",
+        "link": "https://www.facebook.com/19292868552/posts/10150189643478553"
+    },
+    {
+        "name": "Like",
+        "link": "https://www.facebook.com/19292868552/posts/10150189643478553"
+    },
+    {
+        "name": "Share",
+        "link": "http://networkedblogs.com/hGWk3?a=share"
+    }
+],
+    "privacy": {
+    "value": "",
+        "description": "",
+        "friends": "",
+        "allow": "",
+        "deny": ""
+},
+    "type": "link",
+    "status_type": "published_story",
+    "application": {
+    "name": "NetworkedBlogs",
+        "namespace": "blognetworks",
+        "id": "9953271133"
+},
+    "created_time": "2011-05-10T18:35:38+0000",
+    "updated_time": "2015-03-21T04:37:42+0000",
+    "shares": {
+    "count": 3
+},
+    "is_hidden": false,
+    "is_expired": false,
+    "likes": {
+    "data": [
+        {
+            "id": "396918693797571",
+            "name": "Nghĩa Trần"
+        },
+        {
+            "id": "1426829700899497",
+            "name": "Ammar Allia"
+        },
+        {
+            "id": "1499566966984245",
+            "name": "YeongHwan Jeon"
+        },
+        {
+            "id": "488920151250319",
+            "name": "Repins Antons"
+        },
+        {
+            "id": "10204660075117668",
+            "name": "Al Essio Morotti"
+        },
+        {
+            "id": "988283204530480",
+            "name": "Ravi Teja Maddirala"
+        },
+        {
+            "id": "10152286288925834",
+            "name": "Martin Stoyanov"
+        },
+        {
+            "id": "783971228287504",
+            "name": "Sevillejo Jomar"
+        },
+        {
+            "id": "10152509997362423",
+            "name": "Yourim Yi"
+        },
+        {
+            "id": "10203164416218198",
+            "name": "Ger Cugli"
+        },
+        {
+            "id": "771707232876736",
+            "name": "Ijaj Mulani"
+        },
+        {
+            "id": "390603591081088",
+            "name": "Marcos Riley Hawk"
+        },
+        {
+            "id": "10202864784612805",
+            "name": "Mahalakshmi Narayanan"
+        },
+        {
+            "id": "326821207521474",
+            "name": "Roman Yegorov"
+        },
+        {
+            "id": "10152820539922287",
+            "name": "Andrés Martínez Soto"
+        },
+        {
+            "id": "10153301459224447",
+            "name": "Steve Gato"
+        },
+        {
+            "id": "10103636484852013",
+            "name": "Spencer Ahrens"
+        },
+        {
+            "id": "778468045558044",
+            "name": "Ankit Sharma"
+        },
+        {
+            "id": "366852753473914",
+            "name": "CT AL Đa"
+        },
+        {
+            "id": "460673600730715",
+            "name": "Angga DosSantos"
+        },
+        {
+            "id": "444779742334726",
+            "name": "Tahmeem Irshad"
+        },
+        {
+            "id": "605051862918346",
+            "name": "Jeffy Toedjoehsembilanpoeloeh"
+        },
+        {
+            "id": "522287714540350",
+            "name": "Fata Adi Faizal"
+        },
+        {
+            "id": "1530286113882843",
+            "name": "Bang Ojan"
+        },
+        {
+            "id": "642141002527981",
+            "name": "John Noe Petrucci"
+        }
+    ],
+        "paging": {
+        "cursors": {
+            "after": "NjQyMTQxMDAyNTI3OTgx",
+                "before": "Mzk2OTE4NjkzNzk3NTcx"
+        },
+        "next": "https://graph.facebook.com/v2.3/19292868552_10150189643478553/likes?limit=25&after=NjQyMTQxMDAyNTI3OTgx"
+    }
+},
+    "comments": {
+    "data": [
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776161383",
+            "from": {
+                "id": "687091908022482",
+                "name": "Whdtabbasiwahd Abbas"
+            },
+            "message": "ok",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:36:29+0000",
+            "like_count": 437,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776171363",
+            "from": {
+                "id": "689111184469045",
+                "name": "Nico Poschmann"
+            },
+            "message": "second :)",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:36:54+0000",
+            "like_count": 1,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776166373",
+            "from": {
+                "id": "729830270401702",
+                "name": "Punam Pal Shukla"
+            },
+            "message": "k!!",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:37:28+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776176353",
+            "from": {
+                "id": "635545146526717",
+                "name": "Tahsin Keleş"
+            },
+            "message": "upss :D",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:37:28+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776186333",
+            "from": {
+                "id": "841284642564676",
+                "name": "Sumit Sehgal"
+            },
+            "message": "it's great.........",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:38:22+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776181343",
+            "from": {
+                "id": "10202135860733839",
+                "name": "Ian Juma"
+            },
+            "message": "asap ",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:38:41+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776196313",
+            "from": {
+                "id": "678554085536609",
+                "name": "Semih Türkoglu"
+            },
+            "message": "sizin amınıza koyim emi ibne cocukları picler facenizide sizide sikim",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:39:42+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776191323",
+            "from": {
+                "id": "4127450601379",
+                "name": "Bear B-raw Dhuivetter"
+            },
+            "message": "69 yeesssss lol",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:39:52+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776201303",
+            "from": {
+                "id": "856962870984475",
+                "name": "André Chilcott"
+            },
+            "message": "v",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:40:19+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776221263",
+            "from": {
+                "id": "693079597404388",
+                "name": "اسر محمد"
+            },
+            "message": "هلا",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:41:04+0000",
+            "like_count": 1,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776211283",
+            "from": {
+                "id": "10203555573667092",
+                "name": "Girish Shenoy"
+            },
+            "message": "good",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:41:16+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776216273",
+            "from": {
+                "id": "10202756213359044",
+                "name": "Oğuz Sakız"
+            },
+            "message": "İts greatt.. Cok ıyı yaa",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:42:13+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776236233",
+            "from": {
+                "id": "10203598825916468",
+                "name": "Liakat Patwary"
+            },
+            "message": "thanks",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:44:04+0000",
+            "like_count": 1,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776226253",
+            "from": {
+                "id": "655619144504809",
+                "name": "Mahmut Yılmaz"
+            },
+            "message": "Oğuz kardeş ne demek istiyor bu ? Anladıysan kısa bi açıklama yaparmısın \"",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:44:44+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776231243",
+            "from": {
+                "category": "Comedian",
+                "name": "KOmiK seVeR",
+                "id": "126476464082218"
+            },
+            "message": "tıkla beğen",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:44:58+0000",
+            "like_count": 1,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776241223",
+            "from": {
+                "id": "10202062395096736",
+                "name": "Hamdullah Doğan"
+            },
+            "message": "Bu ne kanka",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:46:32+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776246213",
+            "from": {
+                "id": "713922178670494",
+                "name": "Amy Schwartz"
+            },
+            "message": "Looking great!",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:47:39+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776251203",
+            "from": {
+                "id": "10202062395096736",
+                "name": "Hamdullah Doğan"
+            },
+            "message": "Bu ne oluyor bilen söylesin yaw",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:49:04+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776266173",
+            "from": {
+                "id": "658860004183425",
+                "name": "Jose Marraro"
+            },
+            "message": "Todo lo que se nos brinde como seguridad, bienvenido sea.",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:52:00+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776261183",
+            "from": {
+                "id": "799411103402314",
+                "name": "Jeff Lenhart"
+            },
+            "message": "yay!!! gobbledeegook!",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:53:03+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776271163",
+            "from": {
+                "id": "799411103402314",
+                "name": "Jeff Lenhart"
+            },
+            "message": "blah blah blah server blah blah",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:53:28+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776281143",
+            "from": {
+                "id": "750143481684946",
+                "name": "Iyuzt Adrian Gustiana"
+            },
+            "message": "add me",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:55:32+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776286133",
+            "from": {
+                "id": "711755502197220",
+                "name": "David Brotsky"
+            },
+            "message": "What a joke - your work is not secure from the many people who abuse their power, who work at Facebook itself.",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:55:45+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776276153",
+            "from": {
+                "id": "698833606842854",
+                "name": "WA LA Mugo Hy"
+            },
+            "message": "ha!",
+            "can_remove": false,
+            "created_time": "2011-05-10T18:57:33+0000",
+            "like_count": 0,
+            "user_likes": false
+        },
+        {
+            "id": "19292868552:10150189643478553:10101331650483551_10100267776291123",
+            "from": {
+                "id": "624984454250972",
+                "name": "Chamil Wijesooriya"
+            },
+            "message": "I dunno if this's relevant... https one was very slow... and some fb pages didn't load coreectly like the home page... ",
+            "can_remove": false,
+            "created_time": "2011-05-10T19:07:19+0000",
+            "like_count": 0,
+            "user_likes": false
+        }
+    ],
+        "paging": {
+        "cursors": {
+            "after": "WTI5dGJXVnVkRjlqZFhKemIzSTZNVEF4TlRBeE9EazJOalE0T1RnMU5UTTZNVFl5TVRFek5qQT0=",
+                "before": "WTI5dGJXVnVkRjlqZFhKemIzSTZNVEF4TlRBeE9EazJORE00T0RnMU5UTTZNVFl5TVRBNE1EZz0="
+        },
+        "next": "https://graph.facebook.com/v2.3/19292868552_10150189643478553/comments?limit=25&after=WTI5dGJXVnVkRjlqZFhKemIzSTZNVEF4TlRBeE9EazJOalE0T1RnMU5UTTZNVFl5TVRFek5qQT0="
+    }
+}
+}
