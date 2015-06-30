cc58f76a1ce9a06226d7450df17eb404d391b8d0
Andrius Aucinas <andrius@smart-e.org>
2015-06-30 09:54:08 +0100

    accidentally left commented out test

--- src/test/scala/ModelSpecThings.scala
+++ src/test/scala/ModelSpecThings.scala
@@ -45,56 +45,56 @@ class ModelSpecThings extends Specification with AfterAll {
     }
   }
 
-//  "Things tables" should {
-//    db.withSession { implicit session =>
-//      "be empty" in {
-//        val result = ThingsThing.run
-//        result must have size (0)
-//      }
-//      "accept data" in {
-//
-//        val thingsthingRow = new ThingsThingRow(1, LocalDateTime.now(), LocalDateTime.now(), "Test")
-//        val thingId = (ThingsThing returning ThingsThing.map(_.id)) += thingsthingRow
-//
-//        val findthingId = ThingsThing.filter(_.name === "Cover").map(_.id).run.head
-//        val findpropertyId = SystemProperty.filter(_.name === "cover").map(_.id).run.head
-//        val findfieldId = DataField.filter(_.name === "cover").map(_.id).run.head
-//        val findrecordId = DataRecord.filter(_.name === "cover").map(_.id).run.head
-//
-//        val relationshiptype = Some("Relationship description")
-//
-//        val thingsthingtothingcrossrefRow = new ThingsThingtothingcrossrefRow(1, LocalDateTime.now(), LocalDateTime.now(), findthingId, findthingId, relationshiptype)
-//        val thingsthingtothingcrossrefId = (ThingsThingtothingcrossref returning ThingsThingtothingcrossref.map(_.id)) += thingsthingtothingcrossrefRow
-//
-//        val description = Some("An example SystemUnitofmeasurement")
-//
-//        val thingssystempropertystaticcrossrefRow = new ThingsSystempropertystaticcrossrefRow(1, LocalDateTime.now(), LocalDateTime.now(), findthingId, findpropertyId, findfieldId, findrecordId, relationshiptype, true)
-//        val thingssystempropertystaticcrossrefId = (ThingsSystempropertystaticcrossref returning ThingsSystempropertystaticcrossref.map(_.id)) += thingssystempropertystaticcrossrefRow
-//
-//        val thingssystempropertydynamiccrossrefRow = new ThingsSystempropertydynamiccrossrefRow(1, LocalDateTime.now(), LocalDateTime.now(), findthingId, findpropertyId, findfieldId, relationshiptype, true)
-//        val thingssystempropertydynamiccrossrefId = (ThingsSystempropertydynamiccrossref returning ThingsSystempropertydynamiccrossref.map(_.id))
-//
-//        ThingsThing += thingsthingRow
-//
-//        val result = ThingsThing.run
-//        result must have size (1)
-//      }
-//      "allow data to be removed" in {
-//        ThingsThing.delete
-//        ThingsThing.run must have size (0)
-//
-//        ThingsThingtothingcrossref.delete
-//        ThingsThingtothingcrossref.run must have size (0)
-//
-//        ThingsSystempropertydynamiccrossref.delete
-//        ThingsSystempropertydynamiccrossref.run must have size (0)
-//
-//        ThingsSystempropertystaticcrossref.delete
-//        ThingsSystempropertystaticcrossref.run must have size (0)
-//
-//      }
-//    }
-//  }
+  "Things tables" should {
+    db.withSession { implicit session =>
+      "be empty" in {
+        val result = ThingsThing.run
+        result must have size (0)
+      }
+      "accept data" in {
+
+        val thingsthingRow = new ThingsThingRow(1, LocalDateTime.now(), LocalDateTime.now(), "Test")
+        val thingId = (ThingsThing returning ThingsThing.map(_.id)) += thingsthingRow
+
+        val findthingId = ThingsThing.filter(_.name === "Cover").map(_.id).run.head
+        val findpropertyId = SystemProperty.filter(_.name === "cover").map(_.id).run.head
+        val findfieldId = DataField.filter(_.name === "cover").map(_.id).run.head
+        val findrecordId = DataRecord.filter(_.name === "cover").map(_.id).run.head
+
+        val relationshiptype = Some("Relationship description")
+
+        val thingsthingtothingcrossrefRow = new ThingsThingtothingcrossrefRow(1, LocalDateTime.now(), LocalDateTime.now(), findthingId, findthingId, relationshiptype)
+        val thingsthingtothingcrossrefId = (ThingsThingtothingcrossref returning ThingsThingtothingcrossref.map(_.id)) += thingsthingtothingcrossrefRow
+
+        val description = Some("An example SystemUnitofmeasurement")
+
+        val thingssystempropertystaticcrossrefRow = new ThingsSystempropertystaticcrossrefRow(1, LocalDateTime.now(), LocalDateTime.now(), findthingId, findpropertyId, findfieldId, findrecordId, relationshiptype, true)
+        val thingssystempropertystaticcrossrefId = (ThingsSystempropertystaticcrossref returning ThingsSystempropertystaticcrossref.map(_.id)) += thingssystempropertystaticcrossrefRow
+
+        val thingssystempropertydynamiccrossrefRow = new ThingsSystempropertydynamiccrossrefRow(1, LocalDateTime.now(), LocalDateTime.now(), findthingId, findpropertyId, findfieldId, relationshiptype, true)
+        val thingssystempropertydynamiccrossrefId = (ThingsSystempropertydynamiccrossref returning ThingsSystempropertydynamiccrossref.map(_.id))
+
+        ThingsThing += thingsthingRow
+
+        val result = ThingsThing.run
+        result must have size (1)
+      }
+      "allow data to be removed" in {
+        ThingsThing.delete
+        ThingsThing.run must have size (0)
+
+        ThingsThingtothingcrossref.delete
+        ThingsThingtothingcrossref.run must have size (0)
+
+        ThingsSystempropertydynamiccrossref.delete
+        ThingsSystempropertydynamiccrossref.run must have size (0)
+
+        ThingsSystempropertystaticcrossref.delete
+        ThingsSystempropertystaticcrossref.run must have size (0)
+
+      }
+    }
+  }
 
   "Facebook things structures" should {
     db.withSession { implicit session =>
