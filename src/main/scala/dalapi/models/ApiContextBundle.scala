eae460603937982f282dd58a6a19d1b50510be64
AndriusA <andrius.aucinas@gmail.com>
2015-09-28 23:06:53 +0100

    FIXME annotations for contextual bundle models

--- src/main/scala/dalapi/models/ApiContextBundle.scala
+++ src/main/scala/dalapi/models/ApiContextBundle.scala
@@ -4,41 +4,15 @@ import dal.Tables._
 import dalapi.models.ComparisonOperators.ComparisonOperator
 import org.joda.time.LocalDateTime
 
-
-object ComparisonOperators {
-  sealed trait ComparisonOperator
-  case object equal extends ComparisonOperator
-  case object notEqual extends ComparisonOperator
-  case object greaterThan extends ComparisonOperator
-  case object lessThan extends ComparisonOperator
-  case object like extends ComparisonOperator
-  case object dateGreaterThan extends ComparisonOperator
-  case object dateLessThan extends ComparisonOperator
-  case object dateWeekdayGreaterThan extends ComparisonOperator
-  case object dateWeekdayLessThan extends ComparisonOperator
-  case object dateHourGreaterThan extends ComparisonOperator
-  case object dateHourLessThan extends ComparisonOperator
-
-  def fromString(value: String): ComparisonOperator = {
-    Vector(equal, notEqual, greaterThan, lessThan, like,
-      dateGreaterThan, dateLessThan,
-      dateWeekdayGreaterThan, dateWeekdayLessThan,
-      dateHourGreaterThan, dateHourLessThan).find(_.toString == value).get
-  }
-
-  val comparisonOperators: Set[ComparisonOperator] = Set(equal, notEqual, greaterThan, lessThan, like, dateGreaterThan,
-    dateLessThan, dateWeekdayGreaterThan, dateWeekdayLessThan, dateHourGreaterThan, dateHourLessThan)
-}
-
-
 case class ApiBundlePropertySliceCondition(
     id: Option[Int],
-    table: ApiBundlePropertySlice,
+    table: ApiBundlePropertySlice,          // FIXME: linking the wrong way round as discussed on slack
     operator: ComparisonOperator,
     value: String)
 
 object ApiBundlePropertySliceCondition {
-  def fromBundlePropertySliceCondition(condition: BundlePropertySliceConditionRow)(field: ApiDataField) : ApiBundlePropertySliceCondition = {
+  def fromBundlePropertySliceCondition(condition: BundlePropertyliceconditionRow)
+                                      (field: ApiDataField) : ApiBundlePropertySliceCondition = {   // FIXME: ApiDataField?
     new ApiBundlePropertySliceCondition(Some(condition.id),
       table, condition.operator, condition.value,
       ComparisonOperators.fromString(condition.operator))
@@ -49,10 +23,12 @@ case class ApiBundlePropertySlice(
     id: Option[Int],
     dateCreated: Option[LocalDateTime],
     lastUpdated: Option[LocalDateTime],
-    table: ApiDataTable,
-    conditions: Seq[ApiBundleTableCondition])
+    table: ApiDataTable,                      // FIXME: working with properties, not tables here?
+    conditions: Seq[ApiBundleTableCondition]) // FIXME: ApiBundleTableCondition -> ApiBundlePropertySliceCondition
 
 object ApiBundlePropertySlice {
+  // FIXME: working with properties, not tables here?
+  // FIXME: returning ApiBundleTableSlice, should be returning ApiBundlePropertySlice
   def fromBundleTableSlice(slice: BundleTablesliceRow)(table: ApiDataTable) : ApiBundleTableSlice = {
     new ApiBundleTableSlice(Some(slice.id),
       Some(slice.dateCreated), Some(slice.lastUpdated),
@@ -60,13 +36,15 @@ object ApiBundlePropertySlice {
   }
 }
 
+case class ApiSelection
+
 case class ApiEntitySelection(
     id: Option[Int],
     dateCreated: Option[LocalDateTime],
     lastUpdated: Option[LocalDateTime],
-    entityname: String,
-    table: ApiEntity,
-    entitykind: String)
+    entityname: String,                     // FIXME: inconsistent camelCase    FIXME: entityName should be optional
+    table: ApiEntity,                       // FIXME: ApiEntity not defined     FIXME: specific entity should be optional
+    entitykind: String)                     // FIXME: inconsistent camelCase    FIXME: entityKind should be optional
 
 object ApiEntitySelection {
   def fromEntitySelection(entitySelection: EntitySelectionRow)(table: ApiEntity) : ApiEntitySelection = {
@@ -78,16 +56,16 @@ object ApiEntitySelection {
 
 
 case class ApiBundleContext(
-    selftable: ApiBundleContext
+    selftable: ApiBundleContext,                // FIXME: selftable? Should be a list of children bundles
     id: Option[Int],
     dateCreated: Option[LocalDateTime],
     lastUpdated: Option[LocalDateTime],
     name: String,
-    table: ApiEntitySelection,
-    slices: Option[Seq[ApiBundleTableSlice]])
+    table: ApiEntitySelection,                  // FIXME: table?
+    slices: Option[Seq[ApiBundleTableSlice]])   // FIXME: where do ApiBundleTableSlices come here from? At no point in contextual bundles it is dealt directly with tables
 
 object ApiBundleContext {
-  def fromBundleContext(bundleContext: BundleContextRow)(selftable: ApiBundleContext)(table: ApiEntitySelection) : ApiBundleTable = {
+  def fromBundleContext(bundleContext: BundleContextRow)(selftable: ApiBundleContext)(table: ApiEntitySelection) : ApiBundleTable = { // FIXME: returning ApiBundleTable when should be creating ApiBundleContext
     new ApiBundleTable(selftable, Some(bundleContext.id),
       Some(bundleContext.dateCreated), Some(bundleContext.lastUpdated),
       bundleContext.name, table, None)
