37ecac64404c7db8ad99b01fdaedc515a2e06d10
AndriusA <andrius.aucinas@gmail.com>
2015-11-19 17:56:36 +0000

    More Contextual Bundle db remodelling and scala models in progress

--- src/main/scala/hatdex/hat/api/models/ApiBundleContext.scala
+++ src/main/scala/hatdex/hat/api/models/ApiBundleContext.scala
@@ -2,36 +2,30 @@ package hatdex.hat.api.models
 
 import org.joda.time.LocalDateTime
 
-case class ApiEntitySelection(
+case class ApiBundleContextEntitySelection(
     id: Option[Int],
     dateCreated: Option[LocalDateTime],
     lastUpdated: Option[LocalDateTime],
-    entityname: String,                     // FIXME: inconsistent camelCase    FIXME: entityName should be optional
-    table: ApiEntity,                       // FIXME: ApiEntity not defined     FIXME: specific entity should be optional
-    entitykind: String)                     // FIXME: inconsistent camelCase    FIXME: entityKind should be optional
+    entityName: Option[String],
+    entityId: Option[Int],
+    entityKind: Option[String],
+    properties: Option[Seq[ApiBundleContextPropertySelection]])
 
-object ApiEntitySelection {
-  def fromEntitySelection(entitySelection: EntitySelectionRow)(table: ApiEntity) : ApiEntitySelection = {
-    new ApiEntitySelection(Some(entitySelection.id),
-      Some(entitySelection.dateCreated), Some(entitySelection.lastUpdated),
-      entitySelection.name, table, entitykind)
-  }
-}
+case class ApiBundleContextPropertySelection(
+    id: Option[Int],
+    dateCreated: Option[LocalDateTime],
+    lastUpdated: Option[LocalDateTime],
+    propertyRelationshipKind: Option[String],
+    propertyRelationshipId: Option[Int],
+    propertyName: Option[String],
+    propertyType: Option[String],
+    propertyUnitofmeasurement: Option[String])
 
 
 case class ApiBundleContext(
-    selftable: ApiBundleContext,                // FIXME: selftable? Should be a list of children bundles
     id: Option[Int],
     dateCreated: Option[LocalDateTime],
     lastUpdated: Option[LocalDateTime],
     name: String,
-    table: ApiEntitySelection,                  // FIXME: table?
-    slices: Option[Seq[ApiBundleTableSlice]])   // FIXME: where do ApiBundleTableSlices come here from? At no point in contextual bundles it is dealt directly with tables
-
-object ApiBundleContext {
-  def fromBundleContext(bundleContext: BundleContextRow)(selftable: ApiBundleContext)(table: ApiEntitySelection) : ApiBundleTable = { // FIXME: returning ApiBundleTable when should be creating ApiBundleContext
-    new ApiBundleTable(selftable, Some(bundleContext.id),
-      Some(bundleContext.dateCreated), Some(bundleContext.lastUpdated),
-      bundleContext.name, table, None)
-  }
-}
+    entities: Option[Seq[ApiBundleContextEntitySelection]],
+    bundles: Option[Seq[ApiBundleContext]])
