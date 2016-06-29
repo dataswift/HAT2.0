182683a641ae188e69d6404f6b9c98f65d1c8cf2
AndriusA <andrius.aucinas@gmail.com>
2016-06-29 10:09:43 +0100

    data debit value access allowed for owner for all (including expired) data debits

--- src/main/scala/hatdex/hat/authentication/authorization/DataDebitAuthorization.scala
+++ src/main/scala/hatdex/hat/authentication/authorization/DataDebitAuthorization.scala
@@ -9,10 +9,11 @@ object DataDebitAuthorization {
   def hasPermissionAccessDataDebit(dataDebit: Option[DataDebitRow])(implicit user: User): Boolean = {
     dataDebit match {
       case Some(debit) =>
-        (debit.recipientId equals user.userId.toString) &&
+        ((debit.recipientId equals user.userId.toString) &&
           debit.enabled &&
           debit.endDate.isAfter(LocalDateTime.now()) &&
-          debit.startDate.isBefore(LocalDateTime.now())
+          debit.startDate.isBefore(LocalDateTime.now())) ||   // All the above conditions match OR the user is the owner
+        user.role == "owner"
       case None =>
         false
     }
