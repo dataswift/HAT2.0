bb130da12265f06a2c222e2cc01ef08f284b3eca
AndriusA <andrius.aucinas@gmail.com>
2016-10-07 11:57:29 +0100

    updated docker-test-images script to set correct environment variables

--- deployment/docker/docker-build-images.sh
+++ deployment/docker/docker-build-images.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-./hat/docker-build.sh
-./hat-postgres/docker-build.sh
+HAT_HOME=${HAT_HOME:-"$PWD"} #if executing from deployment/  : "$PWD/../.."
+DOCKER=${DOCKER:-"${HAT_HOME}/deployment/docker"}
+
+bash ${DOCKER}/hat/docker-build.sh
+bash ${DOCKER}/hat-postgres/docker-build.sh
