ea3792795371d3491c4dc6ee5f7dcd9b48337bec
andrius <andrius.aucinas@gmail.com>
2017-08-14 15:45:52 +0100

    updates build dependencies
    - forces atlassian JWT libs to be higher version
    - explicitly adds maven release repositories
    - updates resovlers for missing JWT libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,9 +32,10 @@ object Dependencies {
   }
 
   val resolvers = Seq(
+    Resolver.jcenterRepo,
     "Atlassian Releases" at "https://maven.atlassian.com/public/",
-    "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases",
-    "Sonatype snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+    Resolver.bintrayRepo("scalaz", "releases"),
+    Resolver.sonatypeRepo("snapshots"),
     "HAT Library Artifacts Snapshots" at "https://s3-eu-west-1.amazonaws.com/library-artifacts-snapshots.hubofallthings.com",
     "HAT Library Artifacts Releases" at "https://s3-eu-west-1.amazonaws.com/library-artifacts-releases.hubofallthings.com"
   )
