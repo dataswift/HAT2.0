9a89875bd51447eeb86c4ae667f9c290456106a1
Terry Lee <terry.lee@hatdex.org>
2018-09-13 01:37:20 +0800

    Removed unused Imports

--- hat/app/org/hatdex/hat/api/controllers/SystemStatus.scala
+++ hat/app/org/hatdex/hat/api/controllers/SystemStatus.scala
@@ -38,9 +38,7 @@ import play.api.cache.Cached
 import play.api.libs.json._
 import play.api.mvc._
 
-import scala.collection.JavaConverters._
-
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ ExecutionContext }
 
 class SystemStatus @Inject() (
     components: ControllerComponents,
