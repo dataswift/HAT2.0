5e1a4bbf309fe9a1e8b0e1fe7515033fb0eab1a5
Terry Lee <terry.lee@hatdex.org>
2018-12-19 22:42:12 +0000

    Date display in xProfileMapper is wrong. Resulting in since= query not working

--- hat/app/org/hatdex/hat/she/service/DataEndpointMapper.scala
+++ hat/app/org/hatdex/hat/she/service/DataEndpointMapper.scala
@@ -47,6 +47,8 @@ trait DataEndpointMapper extends JodaWrites with JodaReads {
 
   protected def dateFilter(fromDate: Option[DateTime], untilDate: Option[DateTime]): Option[FilterOperator.Operator] = {
     if (fromDate.isDefined) {
+      println(fromDate.get.toString(dateTimeFormat))
+      println(untilDate.get.toString(dateTimeFormat))
       Some(FilterOperator.Between(Json.toJson(fromDate.map(_.toString(dateTimeFormat))), Json.toJson(untilDate.map(_.toString(dateTimeFormat)))))
     }
     else {
@@ -134,7 +136,7 @@ trait DataEndpointMapper extends JodaWrites with JodaReads {
         else {
           Some(s"- ${t.withZone(s.getZone).toString("dd MMMM HH:mm z")}")
         }
-      case (_, Some(t)) if t.hourOfDay().get() == 0 ⇒ None
+      case (_, Some(t)) if t.hourOfDay.get == 0 ⇒ None
       case (Some(s), Some(t))                       ⇒ Some(s"- ${t.withZone(s.getZone).toString("HH:mm z")}")
       case (_, None)                                ⇒ None
     }
@@ -145,8 +147,8 @@ trait DataEndpointMapper extends JodaWrites with JodaReads {
 
 trait FeedItemComparator {
   def extractContent(content: JsValue, tailContent: JsValue, dataKey: String, dataKeyRoot: Option[String] = None): (JsLookupResult, JsLookupResult) = {
-    val tailContentResult = if (dataKeyRoot.isEmpty) (tailContent \ dataKey) else (tailContent \ dataKeyRoot.get \ dataKey)
-    val contentResult = if (dataKeyRoot.isEmpty) (content \ dataKey) else (content \ dataKeyRoot.get \ dataKey)
+    val tailContentResult = if (dataKeyRoot.isEmpty) tailContent \ dataKey else tailContent \ dataKeyRoot.get \ dataKey
+    val contentResult = if (dataKeyRoot.isEmpty) content \ dataKey else content \ dataKeyRoot.get \ dataKey
 
     (contentResult, tailContentResult)
   }
@@ -484,7 +486,7 @@ class FitbitProfileMapper extends DataEndpointMapper with FeedItemComparator {
 
   def mapDataRecord(recordId: UUID, content: JsValue, tailRecordId: Option[UUID] = None, tailContent: Option[JsValue] = None): Try[DataFeedItem] = {
     val comparison = compare(content, tailContent).filter(_._1 == false) // remove all fields that have the same values pre/current
-    if (comparison.length == 0) {
+    if (comparison.isEmpty) {
       Failure(new RuntimeException("Comparision Failure. Data the same"))
     }
     else {
@@ -496,7 +498,7 @@ class FitbitProfileMapper extends DataEndpointMapper with FeedItemComparator {
             Some(contentText), None, None, None))
         }
       } yield {
-        DataFeedItem("fitbit", (content \ "dateCreated").as[DateTime], Seq("profile"),
+        DataFeedItem("fitbit", (tailContent.getOrElse(content) \ "dateCreated").as[DateTime], Seq("profile"),
           Some(title), Some(itemContent), None)
       }
     }
@@ -538,7 +540,7 @@ class TwitterProfileMapper extends DataEndpointMapper with FeedItemComparator {
 
   def mapDataRecord(recordId: UUID, content: JsValue, tailRecordId: Option[UUID] = None, tailContent: Option[JsValue] = None): Try[DataFeedItem] = {
     val comparison = compare(content, tailContent).filter(_._1 == false) // remove all fields that have the same values pre/current
-    if (comparison.length == 0) {
+    if (comparison.isEmpty) {
       Failure(new RuntimeException("Comparision Failure. Data the same"))
     }
     else {
@@ -550,7 +552,7 @@ class TwitterProfileMapper extends DataEndpointMapper with FeedItemComparator {
             Some(contentText), None, None, None))
         }
       } yield {
-        DataFeedItem("twitter", (content \ "lastUpdated").as[DateTime], Seq("profile"),
+        DataFeedItem("twitter", (tailContent.getOrElse(content) \ "lastUpdated").as[DateTime], Seq("profile"),
           Some(title), Some(itemContent), None)
       }
     }
@@ -626,7 +628,7 @@ class FacebookProfileMapper extends DataEndpointMapper with FeedItemComparator {
 
   def mapDataRecord(recordId: UUID, content: JsValue, tailRecordId: Option[UUID] = None, tailContent: Option[JsValue] = None): Try[DataFeedItem] = {
     val comparison = compare(content, tailContent).filter(_._1 == false) // remove all fields that have the same values pre/current
-    if (comparison.length == 0) {
+    if (comparison.isEmpty) {
       Failure(new RuntimeException("Comparision Failure. Data the same"))
     }
     else {
@@ -638,7 +640,7 @@ class FacebookProfileMapper extends DataEndpointMapper with FeedItemComparator {
             Some(contentText), None, None, None))
         }
       } yield {
-        DataFeedItem("facebook", (content \ "updated_time").as[DateTime], Seq("profile"),
+        DataFeedItem("facebook", (tailContent.getOrElse(content) \ "updated_time").as[DateTime], Seq("profile"),
           Some(title), Some(itemContent), None)
       }
     }
