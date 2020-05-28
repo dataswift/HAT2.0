4e3f5f52d3fe5aea0093854d073fcd56916715be
Eleftherios Myteletsis <leymytel@gmail.com>
2020-05-28 21:19:27 +0100

    DS-517 Update hat claim email and messages

--- hat/conf/messages.pl
+++ hat/conf/messages.pl
@@ -1,17 +1,21 @@
-email.dataswift.companyName=Dataswift Ltd
 email.welcome.issuedBy=Your HAT PDA was issued by
 
 email.dataswift.address=8 Comberton Road, Cambridge, CB23 7BA
 email.question=Masz pytanie?
 email.dataswift.copyright=Copyright © 2019 Dataswift Ltd,
-email.dataswift.allRightsReserved=Wszelkie prawa zastrzeżone.
 
 email.hatclaim.subject=Hello from {0}
-email.hatClaim.title={0} wita Cię na Twoim koncie danych osobowych HAT
-email.hatClaim.hatPdaCreated=Konto danych osobowych HAT z ww. adresem URL utworzono na żądanie
-email.hatClaim.safePlace.title=Bezpieczne miejsce Twoich danych
-email.hatClaim.safePlace.description=Konto danych osobowych HAT stanowi miejsce, w którym możesz bezpiecznie przechowywać i posiadać zgodnie z prawem wszystkie zawarte w nim dane.
 email.hatClaim.haveFun.title=Dobrej zabawy z HAT
-email.hatClaim.activateWithinDays=Aktywuj swoje konto w ciągu 7 dni *, aby przeglądać swoje cyfrowe wspomnienia, zobaczyć, gdzie podróżowałeś oraz ściągnąć więcej swoich danych z Internetu.
-email.hatClaim.getStartedNow=Zacznij teraz
-email.hatClaim.offerExpire=* Jeśli nie aktywujesz HAT w ciągu 7 dni, oferta wygaśnie.
+
+email.hatClaim.title={0} welcomes you to your Personal Data Account
+email.hatClaim.hatPdaCreated=A Personal Data Account with the above URL has been created at the request of
+email.hatClaim.goToAppName=Go to {0}
+email.hatClaim.safePlace.title=A safe place for your data
+email.hatClaim.safePlace.description=Your Personal Data Account is where you can safely store and legally own all the data within.
+email.hatClaim.activateYourAccount=Activate your account to continue using {0} and to discover other apps you can use with your PDA.
+email.hatClaim.clickHereToStart=Click here to start
+email.dataswift.pdaProvidedBy=PDA infrastructure provided by:
+email.dataswift.fullAddress=Dataswift Ltd 8 Comberton Road, Cambridge, CB23 7BA
+email.dataswift.companyNumber=Company number: 09821157
+email.dataswift.haveQuestion=Have a question?
+email.dataswift.copyrightAllRightsReserved=Copyright © 2020 Dataswift Ltd, All rights reserved.
