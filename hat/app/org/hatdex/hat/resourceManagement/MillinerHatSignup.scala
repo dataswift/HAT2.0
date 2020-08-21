c67f109f6caa4ec6c4652cb87db733d6d973f962
Ty <tyler.weir@dataswift.io>
2020-08-21 11:30:28 -0400

    DSE-457 #comment revert a useless change.

--- hat/app/org/hatdex/hat/resourceManagement/MillinerHatSignup.scala
+++ hat/app/org/hatdex/hat/resourceManagement/MillinerHatSignup.scala
@@ -39,14 +39,14 @@ trait MillinerHatSignup {
   val ws: WSClient
   val configuration: Configuration
   val schema: String =
-    configuration.get[String]("datacenter.millinerAddress") match {
+    configuration.get[String]("resourceManagement.millinerAddress") match {
       case address if address.startsWith("https") => "https://"
       case address if address.startsWith("http")  => "http://"
       case _                                      => "https://"
     }
 
   val millinerAddress: String = configuration
-    .get[String]("datacenter.millinerAddress")
+    .get[String]("resourceManagement.millinerAddress")
     .stripPrefix("http://")
     .stripPrefix("https://")
   val hatSharedSecret: String =
