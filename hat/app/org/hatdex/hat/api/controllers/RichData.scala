928d118321b72dab6f61f4981c4760926da1d978
Ty <62550845+dataswifty@users.noreply.github.com>
2020-09-09 12:00:48 -0400

    Staging (#167)
    * Correctly match Namespace permissions
    
    * Add more logging.
    
    * Do not use UserRole deserialization.
    
    * Reorganized Contracted Hat Saves.
    
    * Add logging to Adjudicator request.
    
    * Correctly spell address

--- hat/app/org/hatdex/hat/api/controllers/RichData.scala
+++ hat/app/org/hatdex/hat/api/controllers/RichData.scala
@@ -717,49 +717,83 @@ class RichData @Inject() (
     UserAwareAction.async(parsers.json[ContractRequestBody]) {
       implicit request =>
         val contractRequestBody = request.body
-        val dataEndpoint = s"$namespace/$endpoint"
-        val requestIsAllowed = assessRequest(contractRequestBody, namespace)
-
-        requestIsAllowed.flatMap { testResult =>
-          testResult match {
-            case Right(RequestVerified(ns)) => {
-              logger.info(s"RequestVerified: ${ns}")
-              contractRequestBody.body match {
-                case array: JsArray =>
-                  handleJsArray(
-                    contractRequestBody.hatName,
-                    array,
-                    dataEndpoint,
-                    skipErrors
-                  )
-                case value: JsValue =>
-                  handleJsValue(
-                    contractRequestBody.hatName,
-                    value,
-                    dataEndpoint
-                  )
-              }
+        contractRequestBody.body match {
+          // There was no body to store, so fail quickly
+          case None => {
+            logger.error {
+              s"saveContractData included no json body - ns:${namespace} - endpoint:${endpoint}"
             }
-            case Left(x) => {
-              logger.error(
-                s"Contract Save Error: ${x} - ${namespace} - ${endpoint} - ${contractRequestBody}"
-              )
-              Future.successful(NotFound)
+            Future.successful(NotFound)
+          }
+
+          // Body exists, so verify the request and process the body
+          case Some(jsonBody) => {
+            usersService.getUser(contractRequestBody.hatName).flatMap {
+              hatUser =>
+                hatUser match {
+                  case None => {
+                    logger.error {
+                      s"handleJsArray - No user found for ${contractRequestBody.hatName}"
+                    }
+                    Future.successful(BadRequest("No user found."))
+                  }
+                  case Some(hatUser) => {
+                    val dataEndpoint =
+                      s"$namespace/$endpoint"
+                    val requestIsAllowed =
+                      assessRequest(contractRequestBody, namespace)
+
+                    requestIsAllowed.flatMap {
+                      case Right(RequestVerified(ns)) => {
+                        logger.info(s"RequestVerified: ${ns}")
+                        jsonBody match {
+                          case array: JsArray => {
+                            logger.info {
+                              s"saveContractData.body is JsArray: ${jsonBody}"
+                            }
+                            handleJsArray(
+                              hatUser.userId,
+                              array,
+                              dataEndpoint,
+                              skipErrors
+                            )
+                          }
+                          case value: JsValue => {
+                            logger.info {
+                              s"saveContractData.body is JsValue: ${jsonBody}"
+                            }
+                            handleJsValue(
+                              hatUser.userId,
+                              value,
+                              dataEndpoint
+                            )
+                          }
+                        }
+                      }
+                      case Left(x) => {
+                        logger.error {
+                          s"Contract Save Error: ${x} - ${namespace} - ${endpoint} - ${contractRequestBody}"
+                        }
+                        Future.successful(NotFound)
+                      }
+                    } recover {
+                      case e: RichDataDuplicateException =>
+                        BadRequest(Json.toJson(Errors.richDataDuplicate(e)))
+                      case e: RichDataServiceException =>
+                        BadRequest(Json.toJson(Errors.richDataError(e)))
+                      case e: Exception =>
+                        logger.error(e.getMessage)
+                        BadRequest("Contract Data request creation failure.")
+                    }
+                  }
+                }
             }
           }
-        } recover {
-          case e: RichDataDuplicateException =>
-            BadRequest(Json.toJson(Errors.richDataDuplicate(e)))
-          case e: RichDataServiceException =>
-            BadRequest(Json.toJson(Errors.richDataError(e)))
-          case e: Exception =>
-            logger.error(e.getMessage)
-            BadRequest("Contract Data request creation failure.")
         }
     }
 
   def handleJsArray(
-      hatName: String,
+      userId: UUID,
       array: JsArray,
       dataEndpoint: String,
       skipErrors: Option[Boolean]
@@ -767,52 +801,80 @@ class RichData @Inject() (
     ) = {
     val values =
       array.value.map(EndpointData(dataEndpoint, None, None, None, _, None))
-    usersService.getUser(hatName).flatMap { hatUser =>
-      hatUser match {
-        case Some(hatUser) => {
-          dataService
-            .saveData(hatUser.userId, values, skipErrors.getOrElse(false))
-            .map(saved => Created(Json.toJson(saved)))
-        }
-        case None => {
-          Future.successful(BadRequest("No user found."))
-        }
-      }
-    }
+    dataService
+      .saveData(userId, values, skipErrors.getOrElse(false))
+      .map(saved => Created(Json.toJson(saved)))
   }
 
   def handleJsValue(
-      hatName: String,
+      userId: UUID,
       value: JsValue,
       dataEndpoint: String
     )(implicit hatServer: HatServer
     ) = {
     val values = Seq(EndpointData(dataEndpoint, None, None, None, value, None))
-    usersService.getUser(hatName).flatMap { hatUser =>
-      hatUser match {
-        case Some(hatUser) => {
-          dataService
-            .saveData(hatUser.userId, values)
-            .map(saved => Created(Json.toJson(saved.head)))
-        }
-        case None => {
-          Future.successful(BadRequest("No user found."))
-        }
-      }
-    }
+    dataService
+      .saveData(userId, values)
+      .map(saved => Created(Json.toJson(saved.head)))
   }
 
   def verifyNamespace(
       app: Application,
       namespace: String
-    ): Option[String] = {
-    val roles = app.permissions.rolesGranted.map(r =>
-      UserRole.userRoleDeserialize(r.name, r.extra)
+    ): Boolean = {
+    logger.info(s"def verifyNamespace ${namespace} for app ${app}")
+
+    val canReadNamespace = verifyNamespaceRead(app, namespace)
+    val canWriteNamespace = verifyNamespaceWrite(app, namespace)
+
+    logger.info(
+      s"def verifyNamespace read: ${canReadNamespace} - write: ${canWriteNamespace}"
     )
-    if (roles.exists(_.name == namespace))
-      Some(namespace)
-    else
-      None
+
+    (canReadNamespace || canWriteNamespace)
+  }
+
+  def verifyNamespaceRead(
+      app: Application,
+      namespace: String
+    ): Boolean = {
+
+    // ApplicationPermissions(List(NamespaceWrite(samplecontract), NamespaceRead(samplecontract)),None,None)
+    logger.error(
+      s"NamespaceRead: Perms: ${app.permissions} - Namespace: ${namespace}"
+    )
+
+    val rolesOk = app.permissions.rolesGranted.map {
+      case NamespaceRead(namespace) => Some(namespace)
+      case _                        => None
+    }
+    logger.error(
+      s"NamespaceRead: RolesOk: ${rolesOk} - Namespace: ${namespace} - results: ${!rolesOk.flatten.isEmpty}"
+    )
+
+    // flatten removes any Nones, so if this is flattened and .isEmpty then there
+    // was no NamespaceRead found
+    !rolesOk.flatten.isEmpty
+  }
+
+  def verifyNamespaceWrite(
+      app: Application,
+      namespace: String
+    ): Boolean = {
+    logger.info(
+      s"NamespaceWrite: Perms: ${app.permissions} - Namespace: ${namespace}"
+    )
+    val rolesOk = app.permissions.rolesGranted.map {
+      case NamespaceWrite(namespace) => Some(namespace)
+      case _                         => None
+    }
+    logger.info(
+      s"NamespaceWrite: RolesOk: ${rolesOk} - Namespace: ${namespace} - result: ${!rolesOk.flatten.isEmpty}"
+    )
+
+    // flatten removes any Nones, so if this is flattened and .isEmpty then there
+    // was no NamespaceWrite found
+    !rolesOk.flatten.isEmpty
   }
 
   // Convert the basic JSON representation of the ContactRequestBody to the Refined Version
@@ -948,8 +1010,6 @@ class RichData @Inject() (
       }
   }
 
-  /* *** Contract Data *** */
-
   def getContractData(
       namespace: String,
       endpoint: String,
@@ -965,7 +1025,7 @@ class RichData @Inject() (
 
         requestIsAllowed.flatMap { testResult =>
           testResult match {
-            case Right(RequestVerified(ns)) =>
+            case Right(RequestVerified(ns @ _)) =>
               makeData(namespace, endpoint, orderBy, ordering, skip, take)
             case Left(contractError) => {
               logger.error(
@@ -994,9 +1054,12 @@ class RichData @Inject() (
 
     eventuallyMaybeDecision.flatMap { maybeDecision =>
       eventuallyMaybeApp.flatMap { maybeApp =>
+        logger.info(
+          s"AssessRequest: ${maybeDecision} - ${maybeApp} - ${namespace}"
+        )
         decide(maybeDecision, maybeApp, namespace) match {
           case Some(ns) => {
-            logger.error(s"Found a namespace: ${ns}")
+            logger.info(s"Found a namespace: ${ns}")
             Future.successful(
               Right(
                 RequestVerified(s"Token: ${contractRequestBody.contractId}")
@@ -1005,7 +1068,7 @@ class RichData @Inject() (
           }
           case None => {
             logger.error(
-              s"ContractData.assessRequest:Failure ${contractRequestBody} - ${namespace}"
+              s"def assessRequest: decide returned None - ${contractRequestBody} - ${namespace}"
             )
             Future.successful(
               Left(
@@ -1039,13 +1102,21 @@ class RichData @Inject() (
     ): Option[String] = {
     import ContractVerificationSuccess._
 
+    logger.info(
+      s"def decide: decision: ${eitherDecision} - app: ${maybeApp} - ns: ${namespace}"
+    )
+
     (eitherDecision, maybeApp) match {
       case (Right(JwtClaimVerified(_jwtClaim @ _)), Some(app)) => {
-        logger.error(s"jwtclaim verified for ${app}")
-        verifyNamespace(app, namespace)
+        logger.info(s"def decide: JwtClaim verified for app ${app}")
+        if (verifyNamespace(app, namespace)) {
+          Some(namespace)
+        } else {
+          None
+        }
       }
       case (Left(decision), _) => {
-        logger.error(s"ContractData.decide: ${decision}")
+        logger.error(s"def decide: decision: ${decision}")
         None
       }
     }
