7f5c0578cf221ace5eb1da602200dca5fb4b6d41
Ty <tyler.weir@dataswift.io>
2021-02-18 19:23:12 -0500

    DAS-230 #comment Cleaner FileManagerS3Spec

--- hat/test/org/hatdex/hat/api/service/FileManagerS3Spec.scala
+++ hat/test/org/hatdex/hat/api/service/FileManagerS3Spec.scala
@@ -1,90 +1,87 @@
-// /*
-//  * Copyright (C) 2017 HAT Data Exchange Ltd
-//  * SPDX-License-Identifier: AGPL-3.0
-//  *
-//  * This file is part of the Hub of All Things project (HAT).
-//  *
-//  * HAT is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU Affero General Public License
-//  * as published by the Free Software Foundation, version 3 of
-//  * the License.
-//  *
-//  * HAT is distributed in the hope that it will be useful, but
-//  * WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-//  * the GNU Affero General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU Affero General
-//  * Public License along with this program. If not, see
-//  * <http://www.gnu.org/licenses/>.
-//  *
-//  * Written by Andrius Aucinas <andrius.aucinas@hatdex.org>
-//  * 2 / 2017
-//  */
+/*
+ * Copyright (C) 2017 HAT Data Exchange Ltd
+ * SPDX-License-Identifier: AGPL-3.0
+ *
+ * This file is part of the Hub of All Things project (HAT).
+ *
+ * HAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, version 3 of
+ * the License.
+ *
+ * HAT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General
+ * Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Written by Andrius Aucinas <andrius.aucinas@hatdex.org>
+ * 2 / 2017
+ */
 
-// package org.hatdex.hat.api.service
+package org.hatdex.hat.api.service
 
-// import org.hatdex.hat.api.HATTestContext
-// import org.specs2.concurrent.ExecutionEnv
-// import org.specs2.mock.Mockito
-// import play.api.Logger
-// import play.api.test.PlaySpecification
+import org.hatdex.hat.api.HATTestContext
+import play.api.Logger
+import play.api.test.PlaySpecification
 
-// import scala.concurrent.Future
-// import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.duration._
+import io.dataswift.test.common.BaseSpec
+import org.scalatest.{ BeforeAndAfterAll, BeforeAndAfterEach }
 
-// class FileManagerS3Spec(implicit ee: ExecutionEnv) extends PlaySpecification with Mockito with FileManagerContext {
+class FileManagerS3Spec extends BaseSpec with BeforeAndAfterEach with BeforeAndAfterAll with FileManagerContext {
 
-//   val logger = Logger(this.getClass)
+  val logger = Logger(this.getClass)
 
-//   sequential
+  "The `getUploadUrl` method" should "return a signed url for a provided key" in {
+    val fileManager            = application.injector.instanceOf[FileManager]
+    val result: Future[String] = fileManager.getUploadUrl("testFile", None)
 
-//   "The `getUploadUrl` method" should {
-//     "return a signed url for a provided key" in {
-//       val fileManager = application.injector.instanceOf[FileManager]
-//       val result: Future[String] = fileManager.getUploadUrl("testFile", None)
+    val r = Await.result(result, 10.seconds)
+    r must startWith("https://hat-storage-test.s3.eu-west-1.amazonaws.com/hat.hubofallthings.net/testFile")
+  }
 
-//       result must startWith("https://hat-storage-test.s3.eu-west-1.amazonaws.com/hat.hubofallthings.net/testFile").await
-//     }
-//   }
+  "The `getContentUrl` method" should "return a signed url for a provided key" in {
+    val fileManager            = application.injector.instanceOf[FileManager]
+    val result: Future[String] = fileManager.getContentUrl("testFile")
 
-//   "The `getContentUrl` method" should {
-//     "return a signed url for a provided key" in {
-//       val fileManager = application.injector.instanceOf[FileManager]
-//       val result: Future[String] = fileManager.getContentUrl("testFile")
+    val r = Await.result(result, 10.seconds)
+    r must startWith("https://hat-storage-test.s3.eu-west-1.amazonaws.com/hat.hubofallthings.net/testFile")
+  }
 
-//       result must startWith("https://hat-storage-test.s3.eu-west-1.amazonaws.com/hat.hubofallthings.net/testFile").await
-//     }
-//   }
+  "The `deleteContents` method" should "return quietly when deleting any file" in {
+    val fileManager          = application.injector.instanceOf[FileManager]
+    val result: Future[Unit] = fileManager.deleteContents("deleteFile")
 
-//   "The `deleteContents` method" should {
-//     "return quietly when deleting any file" in {
-//       val fileManager = application.injector.instanceOf[FileManager]
-//       val result: Future[Unit] = fileManager.deleteContents("deleteFile")
+    val r = Await.result(result, 10.seconds)
 
-//       result must not(throwAn[Exception]).await
-//       there was one(mockS3client).deleteObject("hat-storage-test", "hat.hubofallthings.net/deleteFile")
-//     }
-//   }
+    //result must not(throwAn[Exception]).await
+    //there was one(mockS3client).deleteObject("hat-storage-test", "hat.hubofallthings.net/deleteFile")
+    fail()
+  }
 
-//   "The `getFileSize` method" should {
-//     "return 0 for files that do not exist" in {
-//       val fileManager = application.injector.instanceOf[FileManager]
-//       val result: Future[Long] = fileManager.getFileSize("nonExistentFile")
+  "The `getFileSize` method" should "return 0 for files that do not exist" in {
+    val fileManager          = application.injector.instanceOf[FileManager]
+    val result: Future[Long] = fileManager.getFileSize("nonExistentFile")
 
-//       result must equalTo(0L).await(3, 10.seconds)
-//     }
+    val r = Await.result(result, 10.seconds)
+    r must equal(0L)
+  }
 
-//     "extract file size for files that do exist" in {
-//       val fileManager = application.injector.instanceOf[FileManager]
-//       val result: Future[Long] = fileManager.getFileSize("testFile")
+  it should "extract file size for files that do exist" in {
+    val fileManager          = application.injector.instanceOf[FileManager]
+    val result: Future[Long] = fileManager.getFileSize("testFile")
 
-//       result must equalTo(123456L).await
-//       there was one(mockS3client).getObjectMetadata("hat-storage-test", "hat.hubofallthings.net/testFile")
-//     }
-//   }
-// }
+    val r = Await.result(result, 10.seconds)
+    r must equal(123456L)
+    //there was one(mockS3client).getObjectMetadata("hat-storage-test", "hat.hubofallthings.net/testFile")
+  }
+}
 
-// trait FileManagerContext extends HATTestContext {
-//   val mockS3client = fileManagerS3Mock.mockS3client
-// }
+trait FileManagerContext extends HATTestContext {
+  val mockS3client = fileManagerS3Mock.mockS3client
+}
--- hat/test/org/hatdex/hat/api/service/richData/RichDataServiceSpec.scala
+++ hat/test/org/hatdex/hat/api/service/richData/RichDataServiceSpec.scala
@@ -260,16 +260,16 @@ class RichDataServiceSpec extends BaseSpec with BeforeAndAfterEach with BeforeAn
     import org.hatdex.hat.dal.Tables._
     import org.hatdex.libs.dal.HATPostgresProfile.api._
 
-    val endpointRecrodsQuery = DataJson.filter(_.source.like("test%")).map(_.recordId)
+    val endpointRecordsQuery = DataJson.filter(_.source.like("test%")).map(_.recordId)
 
     val action = DBIO.seq(
       DataDebitBundle.filter(_.bundleId.like("test%")).delete,
       DataDebitContract.filter(_.dataDebitKey.like("test%")).delete,
       DataCombinators.filter(_.combinatorId.like("test%")).delete,
       DataBundles.filter(_.bundleId.like("test%")).delete,
-      DataJsonGroupRecords.filter(_.recordId in endpointRecrodsQuery).delete,
+      DataJsonGroupRecords.filter(_.recordId in endpointRecordsQuery).delete,
       DataJsonGroups.filterNot(g => g.groupId in DataJsonGroupRecords.map(_.groupId)).delete,
-      DataJson.filter(r => r.recordId in endpointRecrodsQuery).delete
+      DataJson.filter(r => r.recordId in endpointRecordsQuery).delete
     )
 
     Await.result(db.run(action), 60.seconds)
