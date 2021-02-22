f678f092f1abb59be7f371e94220406ccba51e6c
Ty <tyler.weir@dataswift.io>
2021-02-21 20:08:24 -0500

    DAS-230 #comment Update Prometheus Tests.

--- hat/test/org/hatdex/hat/filters/PrometheusFiltersSpec.scala
+++ hat/test/org/hatdex/hat/filters/PrometheusFiltersSpec.scala
@@ -3,116 +3,98 @@ package com.github.stijndehaes.playprometheusfilters.filters
 
 /* Test cases based on the archived project: https://github.com/stijndehaes/play-prometheus-filters */
 
-import org.specs2.mock.Mockito
-import play.api.test.{ FakeRequest, PlaySpecification }
+import javax.inject.Inject
+
+import scala.concurrent.ExecutionContextExecutor
+
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import io.dataswift.test.common.BaseSpec
+import io.prometheus.client.CollectorRegistry
+import play.api.Configuration
+import play.api.libs.typedmap.TypedMap
 import play.api.mvc._
+import play.api.routing.HandlerDef
+import play.api.routing.Router
 import play.api.test.Helpers._
 import play.api.test._
-import play.api.routing.Router
-import io.prometheus.client.CollectorRegistry
-import play.api.libs.typedmap.TypedMap
-import play.api.routing.HandlerDef
-import play.api.mvc.{ AbstractController, ControllerComponents }
-import javax.inject.Inject
-import scala.collection.JavaConverters._
 
-class PrometheusFiltersSpec extends PlaySpecification with Mockito {
-  implicit val system       = ActorSystem()
-  implicit val executor     = system.dispatcher
-  implicit val materializer = ActorMaterializer()
+class PrometheusFiltersSpec extends BaseSpec {
 
-  sequential
+  implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
+  implicit val system: ActorSystem          = ActorSystem()
+  implicit val materializer                 = ActorMaterializer()
 
-  "LatencyFilter" should {
-    "Measure the latency" in {
-      val promFilter: LatencyFilter = new LatencyFilter(mock[CollectorRegistry])
-      val fakeRequest               = FakeRequest()
-      val action                    = new MockController(stubControllerComponents()).ok
+  "LatencyFilter" should "Measure the latency" in {
+    // I needed to have an independnt collector per test, otherwise they complain about having duplicate labels.
+    val registryOne = new CollectorRegistry(true)
+    val promFilter  = new LatencyFilter(registryOne)
+    val fakeRequest = FakeRequest()
+    val action      = new MockController(stubControllerComponents()).ok
 
-      await(promFilter(action)(fakeRequest).run())
+    await(promFilter(action)(fakeRequest).run())
 
-      val metrics = promFilter.requestLatency.collect()
-      metrics must have size 1
-      val samples = metrics.get(0).samples
-
-      val countSample = samples.get(samples.size() - 2)
-      countSample.value mustEqual 1.0
-      countSample.labelValues must have size 0
-    }
+    //val metrics = promFilter.metrics(0).metric.collect()
+    val metrics = promFilter.requestLatency.collect()
+    metrics must have size 1
+    val samples     = metrics.get(0).samples
+    val countSample = samples.get(samples.size() - 2)
+    countSample.value mustBe 1.0
+    countSample.labelValues must have size 0
   }
 
-  "StatusAndRouteLatencyFilter" should {
-    "Measure the latency and status" in {
-      val expectedLabelCount: Long = 5
-      val expectedMethod           = "test"
-      val expectedStatus: String   = "200"
-      val expectedControllerName   = "promController"
-      val expectedPath             = "/path"
-      val expectedVerb             = "GET"
-      val listOfMatches =
-        java.util.Arrays.asList(expectedMethod, expectedStatus, expectedControllerName, expectedPath, expectedVerb)
+  "StatusAndRouteLatencyFilter" should "Measure the latency and status" in {
+    val expectedLabelCount: Long = 6
+    val expectedMethod           = "test"
+    val expectedStatus: String   = "200"
+    val expectedControllerName   = "promController"
+    val expectedPath             = "/path"
+    val expectedVerb             = "GET"
+    val expectedLatencyBucket    = "0.005"
 
-      val promFilter = new StatusAndRouteLatencyFilter(mock[CollectorRegistry])
-      val fakeRequest = FakeRequest().withAttrs(
-        TypedMap(
-          Router.Attrs.HandlerDef -> HandlerDef(null,
-                                                null,
-                                                expectedControllerName,
-                                                expectedMethod,
-                                                null,
-                                                expectedVerb,
-                                                expectedPath,
-                                                null,
-                                                null
-              )
-        )
+    // I needed to have an independnt collector per test, otherwise they complain about having duplicate labels.
+    val registryTwo = new CollectorRegistry(true)
+    val promFilter  = new StatusAndRouteLatencyFilter(registryTwo)
+    val fakeRequest = FakeRequest().withAttrs(
+      TypedMap(
+        Router.Attrs.HandlerDef -> HandlerDef(null, null, "promController", "test", null, "GET", "/path", null, null)
       )
+    )
 
-      val action =
-        new MockController(stubControllerComponents()).ok
+    val action =
+      new MockController(stubControllerComponents()).ok
 
-      await(promFilter(action)(fakeRequest).run())
+    await(promFilter(action)(fakeRequest).run())
 
-      val metrics = promFilter.requestLatency.collect()
-      metrics must have size 1
-      val samples     = metrics.get(0).samples
-      val countSample = samples.get(samples.size() - 2)
+    val metrics = promFilter.requestLatency.collect()
+    metrics must have size 1
+    val samples = metrics.get(0).samples
 
-      countSample.value mustEqual 1.0
-      countSample.labelValues.size mustEqual expectedLabelCount
-      countSample.labelValues mustEqual listOfMatches
-    }
+    samples.get(0).value mustBe 1.0
+    samples.get(0).labelValues must have size expectedLabelCount
+    samples.get(0).labelValues.get(0) mustBe expectedMethod
+    samples.get(0).labelValues.get(1) mustBe expectedStatus
+    samples.get(0).labelValues.get(2) mustBe expectedControllerName
+    samples.get(0).labelValues.get(3) mustBe expectedPath
+    samples.get(0).labelValues.get(4) mustBe expectedVerb
+    samples.get(0).labelValues.get(5) mustBe expectedLatencyBucket
   }
 
-  "StatusCounterFilter" should {
-    "Count the requests with status" in {
-      val promFilter  = new StatusCounterFilter(mock[CollectorRegistry])
-      val fakeRequest = FakeRequest()
-      val actionOK    = new MockController(stubControllerComponents()).ok
-      val actionError = new MockController(stubControllerComponents()).error
+  "StatusCounterFilter" should "Count the requests with status" in {
+    // I needed to have an independnt collector per test, otherwise they complain about having duplicate labels.
+    val registryThree = new CollectorRegistry(true)
+    val promFilter    = new StatusCounterFilter(registryThree)
+    val fakeRequest   = FakeRequest()
+    val action        = new MockController(stubControllerComponents()).ok
 
-      val bothRequests = for {
-        ok <- promFilter(actionOK)(fakeRequest).run()
-        ko <- promFilter(actionError)(fakeRequest).run()
-      } yield (ok, ko)
+    await(promFilter(action)(fakeRequest).run())
 
-      await(bothRequests)
-
-      val metrics = promFilter.requestCounter.collect()
-      metrics.size mustEqual 1
-      val samplesOk    = metrics.get(0).samples.get(0)
-      val samplesError = metrics.get(0).samples.get(1)
-
-      samplesOk.value mustEqual 1.0
-      samplesOk.labelValues.size mustEqual 1
-      samplesOk.labelValues.get(0) mustEqual "200"
-
-      samplesError.value mustEqual 1.0
-      samplesError.labelValues.size mustEqual 1
-      samplesError.labelValues.get(0) mustEqual "404"
-    }
+    val metrics = promFilter.requestCounter.collect()
+    metrics must have size 1
+    val samples = metrics.get(0).samples
+    samples.get(0).value mustBe 1.0
+    samples.get(0).labelValues must have size 1
+    samples.get(0).labelValues.get(0) mustBe "200"
   }
 }
 
