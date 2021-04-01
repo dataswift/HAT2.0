84f5d8e848eb3dbf3b056075ae895950d723dcb3
Scala Steward <me@scala-steward.org>
2021-04-01 18:32:21 +0200

    Update mockito-3-4 to 3.2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -114,7 +114,7 @@ object Dependencies {
     }
 
     object ScalaTest {
-      val scalaplaytestmock = "org.scalatestplus" %% "mockito-3-4"  % "3.2.6.0" % Test
+      val scalaplaytestmock = "org.scalatestplus" %% "mockito-3-4"  % "3.2.7.0" % Test
       val mockitoCore       = "org.mockito"        % "mockito-core" % "3.4.6"   % Test
     }
 
