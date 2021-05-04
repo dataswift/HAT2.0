989bc9dc4daae09a0a8de8eabee26a5ef818ba37
Ty <tyler.weir@dataswift.io>
2021-05-04 16:45:01 -0400

    DAS-518 Update the auth method for HAT talking to Auth Service.

--- hat/app/org/hatdex/hat/client/AuthServiceWsClient.scala
+++ hat/app/org/hatdex/hat/client/AuthServiceWsClient.scala
@@ -224,7 +224,7 @@ class AuthServiceWsClient @Inject() (
       hatDeviceClaim <- maybeHatDeviceClaim
       token: JwtClaim = hatDeviceClaimBuilder.build(hatDeviceClaim)
       encoded         = Jwt.encode(JwtHeader(JwtAlgorithm.HS256), token, secret.value)
-      request         = ws.url(url).withHttpHeaders("Authorization" -> s"Bearer ${encoded}")
+      request         = ws.url(url).withHttpHeaders("X-Auth-Token" -> s"${encoded}")
     } yield request
   }
 }
