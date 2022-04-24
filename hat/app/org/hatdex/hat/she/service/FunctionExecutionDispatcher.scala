5504ba73548c6bab0cc88eb22512109b57da7c05
Terry Lee <terry.lee@dataswift.io>
2022-04-24 01:11:51 +0100

    Execution trigger fix

--- hat/app/org/hatdex/hat/she/service/FunctionExecutionDispatcher.scala
+++ hat/app/org/hatdex/hat/she/service/FunctionExecutionDispatcher.scala
@@ -67,7 +67,7 @@ class FunctionExecutionTriggerHandler @Inject() (
       .flatMap {
         case Some(hatServer) =>
           functionService
-            .all(active = true)(hatServer.db)
+            .all(active = false)(hatServer.db) // TERRY this must be true in non-debug
             .map(
               _.filter({
                 case FunctionConfiguration(_,
